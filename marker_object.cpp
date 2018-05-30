95d69689ad904a457e1abd7118362515253b4635
jobino <jobino-araujo@hotmail.com>
2018-05-30 15:04:34 -0300

    Read video and set rectangle

--- marker_object.cpp
+++ marker_object.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <stdio.h>
+#include <string.h>
 #include <iostream>
 
 
@@ -31,38 +32,36 @@ int main(int argc, char** argv)
     int lineType = LINE_AA; // change it to LINE_8 to see non-antialiased graphics
     int i, width = 1000, height = 700;
     int x1 = -width/2, x2 = width*3/2, y1 = -height/2, y2 = height*3/2;
+    const char videoFile[] = "resources/video.avi";
+    VideoCapture capture(videoFile);
+    Mat frame;
    
     Mat image = Mat::zeros(height, width, CV_8UC3);
     Size textsize = getTextSize("OpenCV forever!", FONT_HERSHEY_COMPLEX, 3, 5, 0);
     Point org((width - textsize.width)/2, (height - textsize.height)/2);
     RNG rng(0xFFFFFFFF);
     Point pt1, pt2;
-    
-    
-    Mat image2;
-    
-    for (i = 0; i < NUMBER; i++)
+
+    if(!capture.isOpened())
+        throw "Error when reading steam_avi";
+
+    namedWindow("Detect Objects", 1);
+    for( ; ; )
     {
-      
       pt1.x = rng.uniform(x1, x2);
       pt1.y = rng.uniform(y1, y2);
       pt2.x = rng.uniform(x1, x2);
       pt2.y = rng.uniform(y1, y2);
       int thickness = rng.uniform(-3, 10);
 
-      //rectangle( image, pt1, pt2, randomColor(rng), 1, lineType );
-      image = imread("traffic_car.jpg");
-      SetDrawingObj(image, pt1.x, pt1.y, 100, 100);
-
-      imshow(wndname, image);
-      waitKey(5);
+      capture >> frame;
+      if(frame.empty())
+        break;
+        SetDrawingObj(frame, pt1.x, pt1.y, 100, 100);
+        imshow("Detect Objects", frame);
+        waitKey(20); // waits to display frame
     }
-
-    
-    SetDrawingObj(image, pt1.x, pt1.y, 100, 100);
-
-
-    imshow(wndname, image);
+   
     waitKey();
     return 0;
 }
