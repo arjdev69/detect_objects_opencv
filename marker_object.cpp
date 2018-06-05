9bf9d8e77db6cae5da44fa6a61314a464f29e092
jobino <jobino-araujo@hotmail.com>
2018-06-05 16:36:14 -0300

    Brackets done - set axis x and y

--- marker_object.cpp
+++ marker_object.cpp
@@ -6,82 +6,96 @@
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/videostab.hpp"
-#include <stdexcept>    
+#include <stdexcept>
 #include "opencv2/opencv_modules.hpp"
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
 
-
 using namespace cv;
 
-void SetDrawingObjLine(Mat img, int x, int y, int w, int h){
-  line(img, Point(w, h), Point(x,y), Scalar( 110, 0, 0 ),2,8);
+void SetDrawingObjLine(Mat img, int x, int y, int w, int h)
+{
+  line(img, Point(w, h), Point(x, y), Scalar(110, 0, 0), 2, 8);
 }
 
-void SetLine(Mat imgLineLeft, int x, int y, int w, int h, bool right){
-  int poseY = 100;  int poseX = 20; 
-  int poseYu = 100; int poseW = 0;
+void SetLine(Mat imgLine, int left, int down, bool right)
+{
+  int y = 160;
+  int x = 140;
+  int w = 160;
+  int h = 160;
+
+  int poseY = 100;
+  int poseX = 20;
+  int poseYu = 100;
+  int poseW = 0;
   int poseHu = 100;
 
-  if(right){
+  if (right)
+  {
     //std::cout<<"Entrou 2 vezes ? "<< right <<std::endl;
     poseX = 0;
     poseW = -20;
   }
 
-  SetDrawingObjLine(imgLineLeft, x, y, w, h);
-  SetDrawingObjLine(imgLineLeft, x + poseX, y + poseY, w + poseW, h);
-  SetDrawingObjLine(imgLineLeft, x, y + poseYu, w, h + poseHu);
+  SetDrawingObjLine(imgLine, x + left, y + down, w + left, h + down);
+  SetDrawingObjLine(imgLine, (x + poseX) + left, (y + poseY) + down, (w + poseW) + left, h + down);
+  SetDrawingObjLine(imgLine, x + left, (y + poseYu) + down, w + left, (h + poseHu) + down);
 }
 
-static Scalar randomColor(RNG& rng)
+void SetDrawingBrackets(Mat imgLine, int left, int down)
 {
-    int icolor = (unsigned)rng;
-    return Scalar(icolor&255, (icolor>>8)&255, (icolor>>16)&255);
+  SetLine(imgLine,left, down, true); // left
+
+  SetLine(imgLine, 80 + left, down, false); // right
 }
 
-int main(int argc, char** argv)
+static Scalar randomColor(RNG &rng)
+{
+  int icolor = (unsigned)rng;
+  return Scalar(icolor & 255, (icolor >> 8) & 255, (icolor >> 16) & 255);
+}
+
+int main(int argc, char **argv)
 {
 
-    char wndname[] = "Drawing Demo";
-    const int NUMBER = 5000;
-    const int DELAY = 5;
-    int lineType = LINE_AA; // change it to LINE_8 to see non-antialiased graphics
-    int i, width = 1000, height = 700;
-    int x1 = -width/2, x2 = width*3/2, y1 = -height/2, y2 = height*3/2;
-    //const char videoFile[] = argv[1];// "resources/video.avi";
+  char wndname[] = "Drawing Demo";
+  const int NUMBER = 5000;
+  const int DELAY = 5;
+  int lineType = LINE_AA; // change it to LINE_8 to see non-antialiased graphics
+  int i, width = 1000, height = 700;
+  int x1 = -width / 2, x2 = width * 3 / 2, y1 = -height / 2, y2 = height * 3 / 2;
+  //const char videoFile[] = argv[1];// "resources/video.avi";
 
-    VideoCapture capture(argv[1]);
-    Mat frame;
-   
-    Mat image = Mat::zeros(height, width, CV_8UC3);
-    Size textsize = getTextSize("OpenCV forever!", FONT_HERSHEY_COMPLEX, 3, 5, 0);
-    Point org((width - textsize.width)/2, (height - textsize.height)/2);
-    RNG rng(0xFFFFFFFF);
-    Point pt1, pt2;
+  VideoCapture capture(argv[1]);
+  Mat frame;
 
-    if(!capture.isOpened())
-        throw "Error when reading steam_avi";
+  Mat image = Mat::zeros(height, width, CV_8UC3);
+  Size textsize = getTextSize("OpenCV forever!", FONT_HERSHEY_COMPLEX, 3, 5, 0);
+  Point org((width - textsize.width) / 2, (height - textsize.height) / 2);
+  RNG rng(0xFFFFFFFF);
+  Point pt1, pt2;
 
-    for( ; ; )
-    {
-      pt1.x = rng.uniform(x1, x2);
-      pt1.y = rng.uniform(y1, y2);
-      pt2.x = rng.uniform(x1, x2);
-      pt2.y = rng.uniform(y1, y2);
-      int thickness = rng.uniform(-3, 10);
+  if (!capture.isOpened())
+    throw "Error when reading steam_avi";
 
-      capture >> frame;
-      if(frame.empty())
-        break;
-        SetLine(frame, 140, 160, 160, 160, true);
+  for (;;)
+  {
+    pt1.x = rng.uniform(x1, x2);
+    pt1.y = rng.uniform(y1, y2);
+    pt2.x = rng.uniform(x1, x2);
+    pt2.y = rng.uniform(y1, y2);
+    int thickness = rng.uniform(-3, 10);
 
-        SetLine(frame, 180, 160, 200, 160, false);
-        imshow("", frame);
-        waitKey(20); // waits to display frame   
-    }
-   
-    waitKey();
-    return 0;
+    capture >> frame;
+    if (frame.empty())
+      break;
+    SetDrawingBrackets(frame, pt2.x, pt2.x);
+    imshow("", frame);
+    waitKey(1); // waits to display frame
+  }
+
+  waitKey();
+  return 0;
 }
--- run_code.sh
+++ run_code.sh
@@ -1,5 +1,5 @@
 g++ -g -Wall -pedantic -std=c++11 marker_object.cpp -o marker `pkg-config --cflags --libs opencv`
 
-
+clear
 
 ./marker ./resources/video.avi
